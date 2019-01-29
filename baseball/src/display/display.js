af3e3fb8f0f736989b7c206bc30e52eb64edf753
dyiar <austin.hl.blake@gmail.com>
2019-01-29 16:14:30 -0700

    added outs to state

--- baseball/src/display/display.js
+++ baseball/src/display/display.js
@@ -5,60 +5,78 @@ class Display extends React.Component {
   state = {
     balls: 0,
     strikes: 0,
-    foul: 0
+    foul: 0,
+    total: 0,
+    outs: 0
   };
 
   handleBalls = event => {
     event.preventDefault();
-    if(this.state.balls === 4) {
-        this.setState({
-            balls: 0,
-            strikes: 0,
-            foul: 0
-          });
-        } else {
-          this.setState({
-            balls: this.state.balls + 1
-          });
-        }
-      };
-
-  handleStrikes = event => {
-    event.preventDefault();
-    if (this.state.strikes === 3) {
+    if (this.state.balls === 3 || this.state.total === 5) {
       this.setState({
         balls: 0,
         strikes: 0,
-        foul: 0
+        foul: 0,
+        total: 0,
       });
     } else {
       this.setState({
-        strikes: this.state.strikes + 1
+        balls: this.state.balls + 1,
+        total: this.state.total + 1
+      });
+    }
+  };
+
+  handleStrikes = event => {
+    event.preventDefault();
+    if (this.state.strikes === 2 || this.state.total === 5) {
+      this.setState({
+        balls: 0,
+        strikes: 0,
+        foul: 0,
+        total: 0,
+        outs: this.state.outs + 1
+      });
+    } else {
+      this.setState({
+        strikes: this.state.strikes + 1,
+        total: this.state.total + 1
       });
     }
   };
 
   handleHit = event => {
-      event.preventDefault();
-      this.setState({
-          balls: 0,
-          strikes: 0,
-          foul: 0
-      })
-  }
+    event.preventDefault();
+    this.setState({
+      balls: 0,
+      strikes: 0,
+      foul: 0,
+      total: 0
+    });
+  };
 
   handleFoul = event => {
-      event.preventDefault();
+    event.preventDefault();
+    if (this.state.strikes === 2) {
       this.setState({
-          foul: this.state.foul +1
-      })
-  }
+        balls: 0,
+        strikes: 0,
+        foul: 0,
+        total: 0,
+        outs: this.state.outs + 1
+      });
+    } else {
+      this.setState({
+        foul: this.state.foul + 1
+      });
+    }
+  };
 
   render() {
     return (
       <>
         <div data-testid="hello">Hello! Welcome to the Ball Park!</div>
-        <div data-testid="balls-total">{this.state.balls}</div>
+        <div data-testid="balls-total">Balls: {this.state.balls}</div>
         <div data-testid="strike-total">Strikes: {this.state.strikes}</div>
         <div data-testid="foul-total">Fouls: {this.state.foul}</div>
         <Dashboard
